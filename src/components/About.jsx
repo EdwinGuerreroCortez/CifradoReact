930102b798f3d334df3acdd0937782476d9d8362
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-10-01 21:28:18 -0600

    add: react

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ function About() {
     return (
         <Box sx={{ maxWidth: 400, margin: 'auto', padding: 2, border: '1px solid #ddd', borderRadius: 2 }}>
             <Typography variant="h4" gutterBottom>Acerca de</Typography>
-            <Typography variant="body1"><strong>Nombre:</strong> Edwin Guerrero Cortez</Typography>
-            <Typography variant="body1"><strong>Matrícula:</strong> 20221030</Typography>
+            <Typography variant="body1"><strong>Nombre:</strong> Edwin Guerrero Cortez, Adair Hernández Escobar</Typography>
+            <Typography variant="body1"><strong>Matrícula:</strong>  20221030, 20221018 </Typography>
             <Typography variant="body1"><strong>Cuatrimestre:</strong> 7° B</Typography>
             <Typography variant="body1"><strong>Carrera:</strong> Ingeniería en Desarrollo y Gestión de Software</Typography>
             <Typography variant="body1"><strong>Docente:</strong> Ing. Ana María Felipe Redondo</Typography>
